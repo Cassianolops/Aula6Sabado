f54d66f29d89728e22623d2110186b0573cadf49
Cassianolops <145858645+Cassianolops@users.noreply.github.com>
2023-10-07 15:30:42 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+
+# frutas = ['maçã','banana','uva', 'cereja',True]
+# frutas.append('melão')
+# print(frutas)
+# frutas.append(12)
+# print(frutas)
+# # frutas.remove('maçã')
+# # # del frutas[0]
+# # print(frutas)
+# # x=len(frutas)
+# # print(x)
+
+# # **Exercício 1:** Crie uma lista chamada `numeros` que contenha os números inteiros de 1 a 5 e imprima-a na tela.
+# numeros = [1,2,3,4,5]
+# print('- Lista\n',numeros)
+# # Exercício 2: Acesse e imprima o terceiro elemento da lista numeros.
+# prim_e = numeros[2]
+# print('- Lista\n',prim_e)
+# # 3 - Adicione o número 6 à lista numeros e imprima a lista atualizada.
+# numeros.append('6')
+# print('- Lista\n',numeros)
+# Exercício 4: Remova o número 3 da lista numeros e imprima a lista resultante.
+# numeros.remove('3')
+# print('- Lista\n',numeros)
+# Exercício 5: Crie uma lista chamada frutas contendo três nomes de frutas diferentes. Em seguida, concatene essa lista com a lista numeros e imprima o resultado.
+# frutas = ['uva','pera','goiaba']
+# # print(numeros,'\n',frutas)
+
+# # **Exercício 6:** Use um loop `for` para percorrer e imprimir todos os elementos da lista `frutas`.
+# for i in frutas:
+#   print(i)
+# **Exercício 7:** Verifique se o número 5 está presente na lista `numeros` e imprima uma mensagem informando se ele está na lista ou não.
+# if 5 in numeros:
+#   print('O número 5 está')
+# else:
+#   print('Não está na lista')
